9ac4b470b6b08daa0bef55c8bc1dc30a38c5b8d8
anglesen1120 <tuan.nguyenminh2507@gmail.com>
2019-09-20 22:00:05 +0700

    2 files modified, 3 added

---
+++ src/app/input-share/input-share.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'input-share',
+  templateUrl: './input-share.component.html',
+  styleUrls: ['./input-share.component.css']
+})
+export class InputShareComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit() {
+   
+  }
+
+}
